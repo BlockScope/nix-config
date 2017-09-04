5062af7c226cd815b307d5aa74e475bd584e5cf6
Phil de Joux <philderbeast@gmail.com>
2017-09-04 11:20:31 -0400

    Refactor only, reduce bulk.

--- overlay.nix
+++ overlay.nix
@@ -1,64 +1,65 @@
 self: super:
 
 let hlib = super.haskell.lib;
+    ft = (subpkg: ~/dev/src/blockscope/flare-timing + "/${subpkg}");
 in
 {
   haskellPackages = super.haskellPackages.override {
-    overrides = hpkgs: _hpkgs: rec {
+    overrides = x: _x: rec {
 
-      fgl = _hpkgs.callCabal2nix "fgl" (super.fetchFromGitHub {
+      fgl = _x.callCabal2nix "fgl" (super.fetchFromGitHub {
         owner = "haskell";
         repo = "fgl";
         rev = "e29503775e474b7a7cd8951b6bebcf1529b888b5";
         sha256 = "0biwsaj6s24l8ix95hkw4syl87ywxy363pr413kazzbhi0csf20s";
       }) {};
 
-      siggy-chardust = self.callPackage ~/dev/src/blockscope/flare-timing/siggy-chardust {};
-      flight-comp = self.callPackage ~/dev/src/blockscope/flare-timing/comp {};
-      flight-kml = self.callPackage ~/dev/src/blockscope/flare-timing/kml {};
-      flight-igc = self.callPackage ~/dev/src/blockscope/flare-timing/igc {};
-      flight-gap = self.callPackage ~/dev/src/blockscope/flare-timing/gap {};
+      siggy-chardust = self.callPackage (ft "siggy-chardust") {};
+      flight-comp = self.callPackage (ft "comp") {};
+      flight-kml = self.callPackage (ft "kml") {};
+      flight-igc = self.callPackage (ft "igc") {};
+      flight-gap = self.callPackage (ft "gap") {};
 
-      flight-units = self.callPackage ~/dev/src/blockscope/flare-timing/units {
-        siggy-chardust = self.haskellPackages.siggy-chardust;
+      flight-units = self.callPackage (ft "units") {
+        siggy-chardust = x.siggy-chardust;
       };
 
-      flight-fsdb = self.callPackage ~/dev/src/blockscope/flare-timing/fsdb {
-        flight-comp = self.haskellPackages.flight-comp;
+      flight-fsdb = self.callPackage (ft "fsdb") {
+        flight-comp = x.flight-comp;
       };
 
-      flight-track = self.callPackage ~/dev/src/blockscope/flare-timing/track {
-        flight-comp = self.haskellPackages.flight-comp;
-        flight-kml = self.haskellPackages.flight-kml;
+      flight-track = self.callPackage (ft "track") {
+        flight-comp = x.flight-comp;
+        flight-kml = x.flight-kml;
       };
 
-      flight-task = self.callPackage ~/dev/src/blockscope/flare-timing/task {
-        fgl = self.haskellPackages.fgl;
-        siggy-chardust = self.haskellPackages.siggy-chardust;
-        flight-units = self.haskellPackages.flight-units;
+      flight-task = self.callPackage (ft "task") {
+        fgl = x.fgl;
+        siggy-chardust = x.siggy-chardust;
+        flight-units = x.flight-units;
       };
 
-      flight-mask = self.callPackage ~/dev/src/blockscope/flare-timing/mask {
-        siggy-chardust = self.haskellPackages.siggy-chardust;
-        flight-units = self.haskellPackages.flight-units;
-        flight-comp = self.haskellPackages.flight-comp;
-        flight-kml = self.haskellPackages.flight-kml;
-        flight-gap = self.haskellPackages.flight-gap;
-        flight-task = self.haskellPackages.flight-task;
-        flight-track = self.haskellPackages.flight-track;
+      flight-mask = self.callPackage (ft "mask") {
+        siggy-chardust = x.siggy-chardust;
+        flight-units = x.flight-units;
+        flight-comp = x.flight-comp;
+        flight-kml = x.flight-kml;
+        flight-gap = x.flight-gap;
+        flight-task = x.flight-task;
+        flight-track = x.flight-track;
       };
 
-      flare-timing = self.callPackage ~/dev/src/blockscope/flare-timing/flare-timing {
-        siggy-chardust = self.haskellPackages.siggy-chardust;
-        flight-units = self.haskellPackages.flight-units;
-        flight-comp = self.haskellPackages.flight-comp;
-        flight-fsdb = self.haskellPackages.flight-fsdb;
-        flight-kml = self.haskellPackages.flight-kml;
-        flight-igc = self.haskellPackages.flight-igc;
-        flight-gap = self.haskellPackages.flight-gap;
-        flight-task = self.haskellPackages.flight-task;
-        flight-mask = self.haskellPackages.flight-mask;
-        flight-track = self.haskellPackages.flight-track;
+      flare-timing = self.callPackage (ft "flare-timing") {
+        siggy-chardust = x.siggy-chardust;
+        flight-units = x.flight-units;
+        flight-comp = x.flight-comp;
+        flight-fsdb = x.flight-fsdb;
+        flight-kml = x.flight-kml;
+        flight-igc = x.flight-igc;
+        flight-gap = x.flight-gap;
+        flight-task = x.flight-task;
+        flight-mask = x.flight-mask;
+        flight-track = x.flight-track;
       };
 
     };
